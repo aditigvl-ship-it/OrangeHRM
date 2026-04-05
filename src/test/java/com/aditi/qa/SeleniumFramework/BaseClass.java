b370da1ebb6ddc65a87b3a778fd04971ad0e216a
Aditi <aditigvl@gmail.com>
2026-04-05 15:23:52 +0530

    Fix BaseClass driver setup and update CI workflow for Extent Reports

--- src/test/java/com/aditi/qa/SeleniumFramework/BaseClass.java
+++ src/test/java/com/aditi/qa/SeleniumFramework/BaseClass.java
@@ -15,43 +15,53 @@ import org.testng.annotations.BeforeMethod;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseClass {
-	
-	 protected Properties prop;
-	 protected WebDriver driver;
-	 
-	 @BeforeMethod
-	 public void setUp() throws IOException {
-	     prop = new Properties();
-	     FileInputStream fis = new FileInputStream(
-	         System.getProperty("user.dir") + "/src/main/resources/config.properties");
-	     prop.load(fis);
+    
+    protected Properties prop;
+    protected WebDriver driver;
+    
+    @BeforeMethod
+    public void setUp() throws IOException {
+        prop = new Properties();
+        FileInputStream fis = new FileInputStream(
+            System.getProperty("user.dir") + "/src/main/resources/config.properties");
+        prop.load(fis);
 
-	     String browser = prop.getProperty("browser").trim();
+        String browser = prop.getProperty("browser").trim();
 
-	     if (browser.equalsIgnoreCase("chrome")) {
-	         WebDriverManager.chromedriver().setup();
-	       
-	       
-	         driver = new ChromeDriver();  
-	     } else if (browser.equalsIgnoreCase("firefox")) {
-	         WebDriverManager.firefoxdriver().setup();
-	         driver = new FirefoxDriver();
-	     } else if (browser.equalsIgnoreCase("edge")) {
-	         WebDriverManager.edgedriver().setup();
-	         driver = new EdgeDriver();
-	     } else {
-	         throw new IllegalArgumentException("Browser Not Supported:" + browser);
-	     }
+        if (browser.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();   // ensure driver matches Chrome
+            ChromeOptions options = new ChromeOptions();
 
-	     driver.manage().window().maximize();
-	     driver.manage().timeouts().implicitlyWait(java.time.Duration.ofSeconds(10));
+            // toggle headless via system property
+            if (System.getProperty("headless", "false").equals("true")) {
+                options.addArguments("--headless=new");
+                options.addArguments("--no-sandbox");
+                options.addArguments("--disable-dev-shm-usage");
+                options.addArguments("--disable-gpu");
+            }
+            options.addArguments("--remote-allow-origins=*");
 
-	     driver.get(prop.getProperty("url").trim());
-	 }
-	 
-	 @AfterMethod
-	 public void tearDown()
-	 {		 driver.quit(); 
-		 
-	 }
+            driver = new ChromeDriver(options);
+
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        } else if (browser.equalsIgnoreCase("edge")) {
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+        } else {
+            throw new IllegalArgumentException("Browser Not Supported: " + browser);
+        }
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(java.time.Duration.ofSeconds(10));
+        driver.get(prop.getProperty("url").trim());
+    }
+    
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
